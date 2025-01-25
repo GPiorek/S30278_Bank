b87d3f7e75b968e93ee2518e299b99feed68d95e
s30278 <s30278@pjwstk.edu.pl>
2025-01-25 10:50:37 +0100

    second commit

---
+++ S30278_Bank/src/main/java/pjwstk/edu/pl/S30278_Bank/S30278BankApplication.java
@@ -0,0 +1,13 @@
+package pjwstk.edu.pl.S30278_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30278BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S30278BankApplication.class, args);
+	}
+
+}
---
+++ S30278_Bank/src/test/java/pjwstk/edu/pl/S30278_Bank/S30278BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pjwstk.edu.pl.S30278_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30278BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
